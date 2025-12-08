1118e8f071c6658ed6aa66a924266bfd317a9033
Davi Martins <davimartins0227@gmail.com>
2025-12-08 20:25:39 -0300

    feat: configura conexao mysql e entidade aeroporto

---
+++ src/main/java/com/puc/aeroportos/domain/aeroporto.java
@@ -0,0 +1,39 @@
+package com.puc.aeroportos.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "aeroporto")
+public class Aeroporto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idAeroporto;
+
+    @Column(nullable = false)
+    private String nomeAeroporto;
+
+    @Column(unique = true, length = 3, nullable = false)
+    private String codigoIata;
+
+    @Column(nullable = false)
+    private String cidade;
+
+    @Column(length = 2, nullable = false)
+    private String codigoPaisIso;
+
+    @Column(nullable = false)
+    private Double latitude;
+
+    @Column(nullable = false)
+    private Double longitude;
+
+    @Column(nullable = false)
+    private Double altitude;
+}
