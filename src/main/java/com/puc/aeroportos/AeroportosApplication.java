58977a24f367d569a294673d8aa29fabe30af8be
Davi Martins <davimartins0227@gmail.com>
2025-12-08 21:55:29 -0300

    chore: adiciona arquivos do projeto que estavam pendentes

---
+++ src/main/java/com/puc/aeroportos/AeroportosApplication.java
@@ -0,0 +1,13 @@
+package com.puc.aeroportos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AeroportosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AeroportosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/puc/aeroportos/AeroportosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.puc.aeroportos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AeroportosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
